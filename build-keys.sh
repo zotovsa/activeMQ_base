b0e2ba24f3fefd980c373fce1d279ebfe4012a98
Administrator <admin@example.com>
2017-03-14 14:30:52 -0500

    make jks file without user interaction

--- build-keys.sh
+++ build-keys.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
+DONAME_NAME=artemis-cluster
 
 
-keytool -genkey -alias localhost  -keyalg RSA  -validity 500 -keypass 12345678 -storepass 12345678  -dname "C=NET, O=ACME.org, OU=IT Division"  -ext "san=ip:127.0.0.1,dns:localhost,uri:https://localhost"  -keystore standalone/configuration/localhost.jks
-
-
-keytool -import -alias root -keystore broker.jks -trustcacerts -file rootCA.crt -storepass 123456
-keytool -genkey -alias broker -keyalg RSA -keystore broker.jks -storepass 123456
+rm broker.jks
+keytool -genkey -alias broker -keyalg RSA -validity 500 -keypass 123456 -storepass 123456 -dname "CN=$DONAME_NAME, OU=Engineering, O=NM, L=Milwaukee S=Winsconsin C=US" -keystore broker.jks
+keytool -import -alias root -keystore broker.jks -trustcacerts -file rootCA.crt -storepass 123456 -noprompt
 keytool -certreq -keyalg RSA -alias broker -file broker.csr -keystore broker.jks  -storepass 123456
 openssl x509 -req -sha256 -in broker.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out broker.crt -days 5000
 keytool -import -alias broker -keystore broker.jks  -file broker.crt -storepass 123456
